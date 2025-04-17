403ff3a7923d990c4c0ce5366f1e49d15064de3b
NyuSD <161166895+NyuSD@users.noreply.github.com>
2025-04-17 11:39:21 -0230

    SQL scripts

--- src/main/resources/scripts.sql
+++ src/main/resources/scripts.sql
@@ -1,43 +1,24 @@
 CREATE TABLE users (
-                       user_id SERIAL PRIMARY KEY,
-                       username VARCHAR(50) NOT NULL,
-                       email VARCHAR(100) NOT NULL UNIQUE,
-                       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    userId SERIAL PRIMARY KEY,
+    userName VARCHAR(50) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(100),
+    address VARCHAR(255),
+    phoneNumber VARCHAR(50),
+    role VARCHAR(20)
 );
 
-CREATE TABLE cars (
-                      car_id SERIAL PRIMARY KEY,
-                      make VARCHAR(50) NOT NULL,
-                      model VARCHAR(50) NOT NULL,
-                      year INT NOT NULL,
-                      price DECIMAL(10, 2) NOT NULL,
-                      seller_id INT NOT NULL,
-                      FOREIGN KEY (seller_id) REFERENCES users(user_id) ON DELETE CASCADE
+CREATE TABLE memberships (
+    membershipId SERIAL PRIMARY KEY,
+    membershipType VARCHAR(50),
+    membershipDescription VARCHAR(255),
+    membershipCost DECIMAL(10,2),
+    memberId INT REFERENCES users(userId)
 );
 
-
-
-CREATE TABLE IF NOT EXISTS public.space_fleet_memberships
-(
-    membership_id SERIAL PRIMARY KEY,
-    membership_tier VARCHAR(50) NOT NULL,
-    membership_credits INTEGER NOT NULL,
-    membership_log TEXT,
-    date_registered DATE DEFAULT CURRENT_DATE,
-    astronaut_id INTEGER NOT NULL,
-    CONSTRAINT space_fleet_memberships_astronaut_fkey FOREIGN KEY (astronaut_id)
-    REFERENCES public.astronauts (astronaut_id)
-    ON UPDATE NO ACTION
-    ON DELETE CASCADE
-    );
-
-TABLESPACE pg_default;
-
-
--- Getting sum for sumberships by month example
-SELECT
-    TO_CHAR(date_purchased, 'YYYY-MM') AS month,
-    SUM(membership_price) AS total_revenue
-FROM public.memberships
-GROUP BY month
-ORDER BY month;
+CREATE TABLE workoutclasses (
+    workoutClassId SERIAL PRIMARY KEY,
+    workoutClassType VARCHAR(50),
+    workoutClassDescription VARCHAR(255),
+    trainerId INT REFERENCES users(userId)
+);
