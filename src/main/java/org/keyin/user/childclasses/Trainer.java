8200f011ced0e07360f3c7b1f008285a443d9d68
NyuSD <161166895+NyuSD@users.noreply.github.com>
2025-04-17 14:35:59 -0230

    Trainer doc support

--- src/main/java/org/keyin/user/childclasses/Trainer.java
+++ src/main/java/org/keyin/user/childclasses/Trainer.java
@@ -2,14 +2,11 @@ package org.keyin.user.childclasses;
 
 import org.keyin.user.User;
 
+/** Subclass whose {@code role} is {@code "trainer"}. */
 public class Trainer extends User {
-    public Trainer() {
-        super();
-        this.setRole("trainer");
-    }
-
-    public Trainer(int userId, String userName, String password, 
-                   String email, String address, String phoneNumber) {
-        super(userId, userName, password, email, address, phoneNumber, "trainer");
+    public Trainer() { super(); setRole("trainer"); }
+    public Trainer(int id, String name, String pw,
+                   String email, String addr, String phone) {
+        super(id, name, pw, email, addr, phone, "trainer");
     }
 }
