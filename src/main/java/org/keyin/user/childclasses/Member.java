e8fd67db442dd7aed5d57639aa841bf49eb04a50
NyuSD <161166895+NyuSD@users.noreply.github.com>
2025-04-17 14:35:45 -0230

    Member doc support

--- src/main/java/org/keyin/user/childclasses/Member.java
+++ src/main/java/org/keyin/user/childclasses/Member.java
@@ -2,14 +2,11 @@ package org.keyin.user.childclasses;
 
 import org.keyin.user.User;
 
+/** Subclass whose {@code role} is {@code "member"}. */
 public class Member extends User {
-    public Member() {
-        super();
-        this.setRole("member");
-    }
-
-    public Member(int userId, String userName, String password,
-                  String email, String address, String phoneNumber) {
-        super(userId, userName, password, email, address, phoneNumber, "member");
+    public Member() { super(); setRole("member"); }
+    public Member(int id, String name, String pw,
+                  String email, String addr, String phone) {
+        super(id, name, pw, email, addr, phone, "member");
     }
 }
